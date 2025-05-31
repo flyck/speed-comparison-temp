bb6c08ff71bcdb5dde6bc764158bc6828f46ae6e
flyck <felix.brilej@googlemail.com>
2025-05-31 20:16:52 +0200

    init

---
+++ index.ts
@@ -0,0 +1,72 @@
+console.log("Hello via Bun!");
+
+const data = {
+	data: [
+		{
+			__name__: "deployment",
+			region: "us-east-1",
+			stage: "prod",
+			status: "success",
+			branch: "main",
+		},
+		{
+			__name__: "deployment",
+			region: "us-east-1",
+			stage: "test",
+			status: "success",
+			branch: "main",
+		},
+		{
+			__name__: "deployment",
+			region: "us-east-1",
+			stage: "dev",
+			status: "success",
+			branch: "main",
+		},
+	],
+};
+
+function setApproach() {
+	const labelsSet = new Set<string>();
+	for (const variant of data.data) {
+		Object.keys(variant).forEach((key) => {
+			if (key !== "__name__") {
+				labelsSet.add(key);
+			}
+		});
+	}
+	return Array.from(labelsSet);
+}
+
+function constApproach() {
+	const labels: string[] = [];
+
+	for (const variant of data.data) {
+		for (const key of Object.keys(variant)) {
+			if (key !== "__name__" && !labels.includes(key)) {
+				labels.push(key);
+			}
+		}
+	}
+
+	return labels;
+}
+
+function main() {
+	console.log("Starting");
+	let start = new Date().getTime();
+	for (let i = 0; i < 1000000; i++) {
+		setApproach();
+	}
+	let end = new Date().getTime();
+	console.log("Speed in ms setApproach:", { diff: end - start });
+
+	let startB = new Date().getTime();
+	for (let i = 0; i < 1000000; i++) {
+		constApproach();
+	}
+	let endB = new Date().getTime();
+	console.log("Speed in ms const approach:", { diff: endB - startB });
+}
+
+main();
